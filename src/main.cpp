8743debd25a07ec27b75465b1a0d2a1413873edb
julia <julie.tataurova@yandex.ru>
2023-11-06 21:26:44 +0300

    graph

--- src/main.cpp
+++ src/main.cpp
@@ -19,20 +19,20 @@ int main(int argc, char *argv[])
     int ret_val = 0;
     int ind_2 = 0;
     
-    // for(int i = 1; i < 3; i++)
-    // {
-    //     ind_2 = *append(10 * i, &listt, &ret_val);
-    // }
-    // int ind_3 = *append(777, &listt, &ret_val);
-    // insert(444, ind_3, &listt, &ret_val);
-    // for(int i = 1; i < 3; i++)
-    // {
-    //     append(i, &listt, &ret_val);
-    // }
-    // popend(&listt, &ret_val);
-    // pop(ind_2, &listt, &ret_val);
-    // pop(1, &listt, &ret_val);
-    // insert(666, 3, &listt, &ret_val);
+    for(int i = 1; i < 3; i++)
+    {
+        ind_2 = *append(10 * i, &listt, &ret_val);
+    }
+    int ind_3 = *append(777, &listt, &ret_val);
+    insert(444, ind_3, &listt, &ret_val);
+    for(int i = 1; i < 3; i++)
+    {
+        append(i, &listt, &ret_val);
+    }
+    popend(&listt, &ret_val);
+    pop(ind_2, &listt, &ret_val);
+    pop(1, &listt, &ret_val);
+    insert(666, 3, &listt, &ret_val);
 
     printf("digraph{\n");
     print_graph_v3(&listt);
